5064927f9318c55aed90f95612136faa8914d147
lcurtis4 <lcurtis0104@gmail.com>
2021-07-30 03:04:19 -0500

    page jsx build and styled

--- src/components/strat/stratForm.js
+++ src/components/strat/stratForm.js
@@ -128,19 +128,30 @@ export const StratForm = () => {
                         <div className="operators">
                             {operators.map(o => {
                                 if (o.side === newStrategy.sideId)
-                            return <button key={o.id}><img src={o.img} alt={o.name} className="opIcon" key={o.name} /></button>
-                        })}
+                                return <button key={o.id}><img src={o.img} alt={o.name} className="opIcon" key={o.name} /></button>
+                            })}
                         </div>
+                        <div className="confirm">
+                        <label className="operatorSelectionConfirmText" htmlFor="confirmOp">4. Confirm Selected Operators</label>
+                    <button type="radio" value="1" name="confirmOps" id="selecedOps">Confirm</button>  
+                        </div>
+                    
                 </div>
             </>
             <>
-                        <div className="roleDescription" >
-                                <input type="text" className="operatorRole" id="1" placeholder="Insert Operator Role Description here" />
-                                <input type="text" className="operatorRole" id="2" placeholder="Insert Operator Role Description here" />
-                                <input type="text" className="operatorRole" id="3" placeholder="Insert Operator Role Description here" />
-                                <input type="text" className="operatorRole" id="4" placeholder="Insert Operator Role Description here" />
-                                <input type="text" className="operatorRole" id="5" placeholder="Insert Operator Role Description here" />
-                        </div>
+                <div className="roleDescription" >
+                    <label className="operatorRoleDescriptionText" htmlFor="roleDescription">5. Describe Each Selected Operators Role:</label>
+                        <input type="text" className="operatorRole" id="1" placeholder="Insert Operator Role Description here" />
+                        <input type="text" className="operatorRole" id="2" placeholder="Insert Operator Role Description here" />
+                        <input type="text" className="operatorRole" id="3" placeholder="Insert Operator Role Description here" />
+                        <input type="text" className="operatorRole" id="4" placeholder="Insert Operator Role Description here" />
+                        <input type="text" className="operatorRole" id="5" placeholder="Insert Operator Role Description here" />
+                </div>
+            </>
+            <>
+                <div className="saveButton">
+                    <button type="radio" className="saveButton" id="save">Save Strategy</button>
+                </div>
             </>
         </>
     </form>
